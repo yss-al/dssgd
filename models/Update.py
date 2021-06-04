b330134e8d97d7e930c5dcfafdc86b61aa20fff0
lightningkay <ew_34@sina.com>
2021-06-04 23:08:36 +0800

    update local update

--- models/Update.py
+++ models/Update.py
@@ -21,29 +21,40 @@ class DatasetSplit(Dataset):
 
 
 class LocalUpdate(object):
-    def __init__(self, args, dataset=None, idxs=None):
-        self.args = args
+    def __init__(self, local_eps=1, local_bs=1, local_lr=0.1,
+                 device='cpu', args=None, dataset=None, idxs=None, verbose=False):
+        if args is None:
+            self.local_ep = local_eps
+            self.local_bs = local_bs
+            self.lr = local_lr
+            self.device = device
+            self.verbose = verbose
+        else:
+            self.local_ep = args.local_ep
+            self.local_bs = args.local_bs
+            self.lr = args.lr
+            self.device = args.device
+            self.verbose = args.verbose
         self.loss_func = nn.CrossEntropyLoss()
-        self.selected_clients = []
-        self.ldr_train = DataLoader(DatasetSplit(dataset, idxs), batch_size=self.args.local_bs, shuffle=True)
+        self.ldr_train = DataLoader(DatasetSplit(dataset, idxs), batch_size=self.local_bs, shuffle=True)
 
     def train(self, net):
         net.train()
         # train and update
         # optimizer = torch.optim.SGD(net.parameters(), lr=self.args.lr, momentum=self.args.momentum)
-        optimizer = torch.optim.Adagrad(net.parameters(), lr=self.args.lr, lr_decay=1e-7)
+        optimizer = torch.optim.Adagrad(net.parameters(), lr=self.lr, lr_decay=1e-7)
 
         epoch_loss = []
-        for iter in range(self.args.local_ep):
+        for iter in range(self.local_ep):
             batch_loss = []
             for batch_idx, (images, labels) in enumerate(self.ldr_train):
-                images, labels = images.to(self.args.device), labels.to(self.args.device)
+                images, labels = images.to(self.device), labels.to(self.device)
                 net.zero_grad()
                 log_probs = net(images)
                 loss = self.loss_func(log_probs, labels)
                 loss.backward()
                 optimizer.step()
-                if self.args.verbose and batch_idx % 10 == 0:
+                if self.verbose and batch_idx % 10 == 0:
                     print('Update Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                         iter, batch_idx * len(images), len(self.ldr_train.dataset),
                                100. * batch_idx / len(self.ldr_train), loss.item()))
